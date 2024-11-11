a9b9c72f5d6cb49b3a913a9b4d540670765d4451
TERMOPALE\anton <anton.shheglov.1@gmail.com>
2024-11-11 03:40:13 +0300

    Add basic subscriber./MQ

---
+++ src/Notifications/NotificationsHandler/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Notifications.Infrastructure;
+using RabbitMQ.Client;
+
+namespace NotificationsHandler
+{
+	internal class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var host = Host.CreateDefaultBuilder(args)
+				.ConfigureServices((builder, services) =>
+				{
+					var factory = new ConnectionFactory
+					{
+						Uri = new(builder.Configuration.GetConnectionString("RabbitConnection"))
+					};
+					services.AddNotificationConnectionProvider(factory);
+					services.AddHostedService<QueueSubscriberService>();
+				})
+				.Build();
+
+			await host.RunAsync();
+		}
+	}
+}
---
+++ src/Notifications/NotificationsHandler/QueueSubscriberService.cs
@@ -0,0 +1,37 @@
+﻿using API.Abstraction.Notifications;
+using Microsoft.Extensions.Hosting;
+using Notifications.Infrastructure;
+using Notifications.Infrastructure.Provider;
+using ProtoBuf;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace NotificationsHandler
+{
+	public class QueueSubscriberService : BackgroundService
+	{
+		private readonly IConnectionProvider _connectionProvider;
+
+		public QueueSubscriberService(IConnectionProvider connectionProvider)
+		{
+			_connectionProvider = connectionProvider;
+		}
+
+		protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+		{
+			var connection = await _connectionProvider.GetConnection();
+			using var channel = await connection.CreateChannelAsync(cancellationToken: stoppingToken);
+
+			var consumer = new AsyncEventingBasicConsumer(channel);
+			consumer.ReceivedAsync += async (message, arguments) =>
+			{
+				var notification = Serializer.Deserialize<Notification>(arguments.Body);
+				Console.WriteLine(notification.Operation);
+			};
+
+			await channel.BasicConsumeAsync(KnownQueueNames.Booking, true, consumer, cancellationToken: stoppingToken);
+
+			await Task.CompletedTask;
+		}
+	}
+}
